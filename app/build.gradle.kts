f687a3ee4018cc78b7ecca987e9d6d04191b8fdb
xMikkkaa <xMikkkaa@users.noreply.github.com>
2026-07-18 16:01:26 +0800

    feat: update version to 2.5.0 and refresh release binaries
    - **Build**: Bump `versionName` to 2.5.0 in `build.gradle.kts` and `output-metadata.json`.
    - **Release**: Update the release APK and associated baseline profile binaries.
    
    Signed-off-by: xMikkkaa <xMikkkaa@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 36
         versionCode = 2
-        versionName = "2.4.1"
+        versionName = "2.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
