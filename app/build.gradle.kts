4dcd4ef93f7bb5cf0ee3b2adf941ccb814b877a5
xMikkkaa <xMikkkaa@users.noreply.github.com>
2026-07-30 07:03:57 +0800

    chore: bump version to 2.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 36
         versionCode = 2
-        versionName = "2.5.0"
+        versionName = "2.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
